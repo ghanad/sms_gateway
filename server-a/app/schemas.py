6248373f98ee2b33adb4ce032739fe1e2f1391ec
Aghajani <aghajani.ali@gmail.com>
2025-08-22 13:30:25 +0330

    Import Any and Dict types for schema definitions

--- server-a/app/schemas.py
+++ server-a/app/schemas.py
@@ -1,6 +1,6 @@
 import re
 from datetime import datetime
-from typing import List, Optional
+from typing import Any, Dict, List, Optional
 from uuid import UUID, uuid4
 
 from pydantic import BaseModel, Field, validator
