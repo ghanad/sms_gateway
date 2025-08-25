b0f8a3b20bc7457cec145933b2441f58f5af0b32
a.aghajani <aghajani.ali@gmail.com>
2025-08-25 06:11:37 +0000

    add api_key to user

--- server-b/user_management/admin.py
+++ server-b/user_management/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
+from .models import Profile
 
-# Register your models here.
+class ProfileInline(admin.StackedInline):
+    model = Profile
+    can_delete = False
+    verbose_name_plural = 'profile'
+    fk_name = 'user'
+    fields = ('api_key',) # Only show api_key in the inline form
+
+class CustomUserAdmin(UserAdmin):
+    inlines = (ProfileInline,)
+    list_display = UserAdmin.list_display + ('get_api_key',) # Add get_api_key to list display
+
+    def get_api_key(self, instance):
+        return instance.profile.api_key
+    get_api_key.short_description = 'API Key' # Column header
+
+admin.site.unregister(User)
+admin.site.register(User, CustomUserAdmin)
--- server-b/user_management/forms.py
+++ server-b/user_management/forms.py
@@ -1,10 +1,28 @@
 from django import forms
 from django.contrib.auth.forms import UserChangeForm
 from django.contrib.auth import get_user_model
+from .models import Profile # Import the Profile model
 
 User = get_user_model()
 
 class CustomUserChangeForm(UserChangeForm):
+    api_key = forms.CharField(max_length=255, required=False, help_text="API Key for the user.")
+
     class Meta(UserChangeForm.Meta):
         model = User
-        fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser')
+        fields = ('username', 'email', 'first_name', 'last_name', 'is_active', 'is_staff', 'is_superuser', 'api_key')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if self.instance and hasattr(self.instance, 'profile'):
+            self.fields['api_key'].initial = self.instance.profile.api_key
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        if commit:
+            user.save()
+        if hasattr(user, 'profile'):
+            user.profile.api_key = self.cleaned_data['api_key']
+            if commit:
+                user.profile.save()
+        return user
