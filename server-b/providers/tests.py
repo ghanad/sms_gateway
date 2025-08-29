aecff57777c83afbb42788d241913870481c6d04
ghanad <48297368+ghanad@users.noreply.github.com>
2025-08-29 16:37:16 +0330

    Add tests for messaging and provider models

--- server-b/providers/tests.py
+++ server-b/providers/tests.py
@@ -1,3 +1,40 @@
+from django.core.exceptions import ValidationError
 from django.test import TestCase
+from providers.models import SmsProvider, AuthType
 
-# Create your tests here.
+
+class SmsProviderModelTests(TestCase):
+    def _base_provider_kwargs(self, **extra):
+        data = {
+            "name": "Provider",
+            "slug": "provider",
+            "send_url": "http://example.com/send",
+            "balance_url": "http://example.com/balance",
+            "default_sender": "100",
+        }
+        data.update(extra)
+        return data
+
+    def test_basic_auth_requires_password(self):
+        provider = SmsProvider(**self._base_provider_kwargs(
+            auth_type=AuthType.BASIC,
+            auth_config={"username": "u"},
+        ))
+        with self.assertRaises(ValidationError) as ctx:
+            provider.full_clean()
+        self.assertIn("password", ctx.exception.message_dict["auth_config"][0])
+
+    def test_api_key_header_requires_key(self):
+        provider = SmsProvider(**self._base_provider_kwargs(
+            auth_type=AuthType.API_KEY_HEADER,
+            auth_config={},
+        ))
+        with self.assertRaises(ValidationError) as ctx:
+            provider.full_clean()
+        self.assertIn("Missing: key", ctx.exception.message_dict["auth_config"][0])
+
+    def test_str_returns_name(self):
+        provider = SmsProvider(**self._base_provider_kwargs(auth_type=AuthType.NONE))
+        # full_clean should pass without raising
+        provider.full_clean()
+        self.assertEqual(str(provider), "Provider")
