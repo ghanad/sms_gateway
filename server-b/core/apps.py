9d2f3155cd7155b6d0886db787c51647f432cc75
ghanad <48297368+ghanad@users.noreply.github.com>
2025-08-29 10:27:42 +0330

    Broadcast full config state via RabbitMQ

--- server-b/core/apps.py
+++ server-b/core/apps.py
@@ -1,6 +1,19 @@
 from django.apps import AppConfig
+from django.conf import settings
+import os
+import sys
 
 
 class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'core'
+
+    def ready(self):
+        """Start background tasks once Django is ready."""
+        # Avoid running twice with the reloader in development
+        if "test" in sys.argv:
+            return
+        if os.environ.get("RUN_MAIN") == "true" or not settings.DEBUG:
+            from .state_broadcaster import start_periodic_broadcast
+
+            start_periodic_broadcast()
