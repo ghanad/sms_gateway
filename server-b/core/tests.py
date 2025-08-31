36f00e962ad5853078196be5e717e613c1ccdcbe
ghanad <48297368+ghanad@users.noreply.github.com>
2025-08-31 11:46:39 +0330

    Move timezone selection to dedicated settings page

--- server-b/core/tests.py
+++ server-b/core/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+
+class SettingsTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(username='user', password='pass')
+
+    def test_settings_page_has_timezone_select(self):
+        self.client.login(username='user', password='pass')
+        response = self.client.get(reverse('settings'))
+        self.assertContains(response, 'id="timezone-select"')
+
+    def test_dashboard_has_no_timezone_select(self):
+        self.client.login(username='user', password='pass')
+        response = self.client.get(reverse('dashboard'))
+        self.assertNotContains(response, 'id="timezone-select"')
+
--- server-b/core/views.py
+++ server-b/core/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 
+
 @login_required
 def dashboard(request):
     return render(request, 'core/dashboard.html')
+
+
+@login_required
+def settings(request):
+    return render(request, 'core/settings.html')
