0f058059763c6436961bdd73124d123545c0572d
ghanad <48297368+ghanad@users.noreply.github.com>
2025-08-31 13:01:17 +0330

    Add admin message detail view

--- server-b/messaging/urls.py
+++ server-b/messaging/urls.py
@@ -1,10 +1,20 @@
 from django.urls import path
-from .views import UserMessageListView, AdminMessageListView, MessageDetailView
+from .views import (
+    UserMessageListView,
+    AdminMessageListView,
+    MessageDetailView,
+    AdminMessageDetailView,
+)
 
 app_name = 'messaging'
 
 urlpatterns = [
     path('my-messages/', UserMessageListView.as_view(), name='my_messages_list'),
     path('admin-messages/', AdminMessageListView.as_view(), name='admin_messages_list'),
+    path(
+        'admin-messages/<uuid:tracking_id>/',
+        AdminMessageDetailView.as_view(),
+        name='admin_message_detail',
+    ),
     path('messages/<uuid:tracking_id>/', MessageDetailView.as_view(), name='message_detail'),
 ]
